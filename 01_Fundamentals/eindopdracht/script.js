42655a5796f86508bcaf1084ec673f452651a75a
Esther <esther.e.schuitemaker@gmail.com>
2022-06-08 17:18:20 +0200

    new nav

--- 01_Fundamentals/eindopdracht/script.js
+++ 01_Fundamentals/eindopdracht/script.js
@@ -1,7 +1,9 @@
 const hamburger = document.querySelector(".hamburger");
 const navMenu = document.querySelector(".nav-menu");
+// const navItem = document.querySelector(".nav-item-transition");
 
 hamburger.addEventListener("click", () => {
     hamburger.classList.toggle("active");
     navMenu.classList.toggle("active");
+    // navItem.classList.toggle("active");
 })
