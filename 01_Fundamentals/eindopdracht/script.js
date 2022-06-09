9848620e4c6449844ed71d5af702ece5fdc55a08
Esther <esther.e.schuitemaker@gmail.com>
2022-06-09 15:26:06 +0200

    full page

--- 01_Fundamentals/eindopdracht/script.js
+++
@@ -1,9 +0,0 @@
-const hamburger = document.querySelector(".hamburger");
-const navMenu = document.querySelector(".nav-menu");
-// const navItem = document.querySelector(".nav-item-transition");
-
-hamburger.addEventListener("click", () => {
-    hamburger.classList.toggle("active");
-    navMenu.classList.toggle("active");
-    // navItem.classList.toggle("active");
-})
