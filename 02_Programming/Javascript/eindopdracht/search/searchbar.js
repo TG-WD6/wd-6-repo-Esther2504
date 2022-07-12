e907f37a8d8ab30bd4584d6a90f20f0db8080e7a
Esther Schuitemaker <esther252000@gmail.com>
2022-07-12 13:02:59 +0200

    search bar local storage

--- 02_Programming/Javascript/eindopdracht/search/searchbar.js
+++ 02_Programming/Javascript/eindopdracht/search/searchbar.js
@@ -1,25 +1,43 @@
 let inputValues = [];
 
+function saveData(){
+    let searchInput = document.querySelector("#searchbar__input").value;
 
-document.getElementById ("icon").addEventListener ("click", searchFunction, false);
+    if (localStorage.getItem('data') == null) {
+        localStorage.setItem('data', '[]');
+    }
+
+    let savedData = JSON.parse(localStorage.getItem('data'));
+    savedData.push(searchInput);
+
+    localStorage.setItem('data', JSON.stringify(savedData))
+
+// localStorage.setItem("suggestion", searchInput);
+
+// let sug = localStorage.getItem("suggestion")
+
+// let sug2 = document.createElement("p");
+// sug2.textContent = sug;
+// document.body.appendChild(sug2)
+}
+/*
+document.getElementById("searchbar__icon").addEventListener ("click", searchFunction, false);
 
 function searchFunction() {
 
-let input = document.getElementById("searchinput").value;
-inputValues.push(input);
-localStorage.setItem("suggestion",JSON.stringify(inputValues));
+inputValues.push(searchInput);
+localStorage.setItem("suggestion",JSON.stringify(inputValues)); */
 // console.log(localStorage.getItem("suggestion"));
 
-}
+
 // var storedValue = localStorage.getItem("search");
 
 // document.getElementById("searchinput").addEventListener("change", suggestItems());
 
-function suggestItems() {
-    let input = document.getElementById("searchinput").value;
-    inputValues.push(input);
+/* function suggestItems() {
+    inputValues.push(searchInput);
     localStorage.setItem("suggestion",JSON.stringify(inputValues));
-    let suggestBox = document.getElementById("suggestionsbox");
+    let suggestBox = document.getElementById("searchbar__suggestions");
 
     for (i=0; i<inputValues.length; i++) {
         console.log(inputValues[i]);
@@ -27,4 +45,4 @@ function suggestItems() {
     
      }
 
-}
+} */
