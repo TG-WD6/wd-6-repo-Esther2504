98bc8ad1a5e783eb36681c75436d0daa16d9b670
Esther Schuitemaker <esther252000@gmail.com>
2022-07-13 13:48:08 +0200

    verbetering search en clock

--- 02_Programming/Javascript/eindopdracht/script.js
+++ 02_Programming/Javascript/eindopdracht/script.js
@@ -1,42 +1,42 @@
-setInterval(Timer, 1000);
-
-function Timer() {
-    let currentDate = new Date();
-    let currentHour = currentDate.getHours()
-    let currentMinute = currentDate.getMinutes()
-    let currentSecond = currentDate.getSeconds()
-    let time = currentHour + ":" + currentMinute + ":" + currentSecond
-    let myDiv = document.getElementById("clock__digital")
-    myDiv.innerText = time
-}
-
-
 setInterval(moveSeconds, 1000)
 
 function moveSeconds() {
     let currentDate = new Date();
-    let currentSecond = currentDate.getSeconds()
-    let rotateSeconds = (currentSecond / 60) * 360
-    let secondsPointer = document.querySelector(".clock__seconds");
+    let currentSecond = currentDate.getSeconds();
+    let rotateSeconds = (currentSecond / 60) * 360;
+    let secondsPointer = document.querySelector("#clock__seconds");
     secondsPointer.style.transform = "rotate(" + rotateSeconds + "deg)";
 }
-setInterval(moveMinute, 1000)
 
-function moveMinute() {
+setInterval(moveMinutes, 1000)
+
+function moveMinutes() {
     let currentDate = new Date();
-    let currentMinute = currentDate.getMinutes()
-    let rotateMinutes = (currentMinute / 60) * 360
-    let minutesPointer = document.querySelector(".clock__minutes");
+    let currentMinute = currentDate.getMinutes();
+    let rotateMinutes = (currentMinute / 60) * 360;
+    let minutesPointer = document.querySelector("#clock__minutes");
     minutesPointer.style.transform = "rotate(" + rotateMinutes + "deg)";
 }
-setInterval(moveHour, 1000)
 
-function moveHour() {
+setInterval(moveHours, 1000)
+
+function moveHours() {
     let currentDate = new Date();
-    let currentHour = currentDate.getHours()
-    let currentMinute = currentDate.getMinutes()
-    let rotateHour = ((currentHour + "." + currentMinute) / 12) * 360
-    let hourPointer = document.querySelector(".clock__hours");
+    let currentHour = currentDate.getHours();
+    let currentMinute = currentDate.getMinutes();
+    let rotateHour = ((currentHour + "." + currentMinute) / 12) * 360;
+    let hourPointer = document.querySelector("#clock__hours");
     hourPointer.style.transform = "rotate(" + rotateHour + "deg)";
 }
 
+setInterval(Timer, 1000);
+
+function Timer() {
+    let currentDate = new Date();
+    let currentHour = currentDate.getHours();
+    let currentMinute = currentDate.getMinutes();
+    let currentSecond = currentDate.getSeconds();
+    let currentTime = currentHour + ":" + currentMinute + ":" + currentSecond;
+    let timerDiv = document.querySelector("#clock__digital");
+    timerDiv.innerText = currentTime;
+}
--- 02_Programming/Javascript/eindopdracht/search/searchbar.js
+++ 02_Programming/Javascript/eindopdracht/search/searchbar.js
@@ -1,45 +1,55 @@
-let inputValues = [];
-
 let searchInput = document.querySelector("#searchbar__input");
-let inputContainer = document.querySelector(".searchbar__input__container");
-let suggestionBox = document.querySelector("#searchbar__suggestions")
+let inputContainer = document.querySelector("#searchbar__input__container");
+let suggestionBox = document.querySelector("#searchbar__suggestions");
 
-function saveData(){
-
+function saveData() {
     let searchInput = document.querySelector("#searchbar__input").value;
-    if (localStorage.getItem('data') == null) {
-        localStorage.setItem('data', '[]');
+
+    // Als er niets in opgeslagen in de localstorage, wordt er een lege array opgeslagen
+    if (localStorage.getItem('input') == 0) {
+        localStorage.setItem('input', '[]');
     }
 
-    let savedData = JSON.parse(localStorage.getItem('data'));
+    // Met getItem wordt de oude data opgehaald van de webserver, JSON.parse zet de tekst om in leesbare data
+    let savedData = JSON.parse(localStorage.getItem('input'));
+    // De opgeslagen data wordt aan de nieuwe data toegevoegd
     savedData.push(searchInput);
 
-    localStorage.setItem('data', JSON.stringify(savedData))
+    // De data wordt toegevoegd aan de local storage. JSON.stringify zet de data om in tekst zodat het naar een webserver kan worden gestuurd
+    localStorage.setItem('input', JSON.stringify(savedData))
 }
 
 searchInput.addEventListener('keyup', () => {
-    let savedData = JSON.parse(localStorage.getItem('data'));
-let results = [];
-let input = searchInput.value;
-if (input.length) {
-    results = savedData.filter((item) => {
-return item.toLowerCase().includes(input.toLowerCase())
-    })
-}
+    let savedData = JSON.parse(localStorage.getItem('input'));
+    let results = [];
+    let input = searchInput.value;
 
-renderResults(results)
+    if (input.length) {
+        // de results array wordt gelijk gesteld aan de gefilterde localStorage data
+        results = savedData.filter((item) => {
+            // returnt array items die de input bevatten
+            return item.toLowerCase().includes(input.toLowerCase())
+        })
+    }
+
+    // Roept functie aan om de resultaten in op de pagina te laten zien
+    renderResults(results)
 })
 
 
 function renderResults(results) {
-    if (!results.length) {
-        return inputContainer.classList.remove('show');
-    }
-
-    let content = results.map((item) => {
-        return `<li>${item}</li>`
+    // Elke suggestie die matcht, wordt apart teruggegeven
+    // .join voegt de array items aan elkaar zonder er een komma tussen te hebben
+    let suggesties = results.map((item) => {
+        return `${item} \r\n`
     }).join('')
 
+    // De class 'show' wordt toegevoegd aan de inputcontainer zodat je de suggestiebox ziet zodra een suggestie matcht
     inputContainer.classList.add('show');
-    suggestionBox.innerHTML = `<ul>${content}</ul>`
+    suggestionBox.textContent = `${suggesties}`
+
+    // Als er geen items zijn in de localstorage die matchen met de input, wordt de div met suggesties niet getoond
+    if (results.length == 0) {
+        return inputContainer.classList.remove('show');
+    }
 }
